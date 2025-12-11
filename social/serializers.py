9ff34726e30f74ad4724c35156fc74b27e920b11
Alex Mwangi Gacanga <alexgacanga01@gmail.com>
2025-12-11 22:55:06 +0300

    Project Initialization,App creation,Models and Serializers Created

---
+++ social/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework.serializers import Serializer
+from django.forms import fields
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializers(serializers.Serializer):
+    author = serializers.ReadOnlyField(source = 'author.username')
+
+    class Meta:
+        model = Post
+        fields = ['author','content','id','image','created_at','updated_at']
+
+    def validate(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError("Post too long")
+        return value
